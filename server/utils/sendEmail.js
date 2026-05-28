33f0296e5f2af542168d72dc07787ada74ca8d12
Ganesh sharma <gansharma03@gmail.com>
2026-05-28 20:29:16 +0530

    fixed syntax error

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,76 +6,79 @@ async (data) => {
 
   try {
 
-     
+    await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name:
+            "OSR Solutions",
+          email:
+            "osrsolutions51@gmail.com",
+        },
 
-    const response =
-      await axios.post(
-        "https://api.brevo.com/v3/smtp/email",
-        {
-          sender: {
-            name:
-              "OSR Solutions",
+        to: [
+          {
             email:
               "osrsolutions51@gmail.com",
           },
+        ],
 
-          to: [
-            {
-              email:
-                "osrsolutions51@gmail.com",
-            },
-          ],
-
-          replyTo: {
-            email:
-              data.email,
-          },
-
-          subject:
-            "🚀 New Contact Form Submission",
-
-          htmlContent: `
-            <h2>
-              New Customer Inquiry
-            </h2>
-
-            <p><strong>Name:</strong>
-            ${data.name}</p>
-
-            <p><strong>Email:</strong>
-            ${data.email}</p>
-
-            <p><strong>Phone:</strong>
-            ${data.phone}</p>
-
-            <p><strong>Service:</strong>
-            ${data.service}</p>
-
-            <p><strong>Message:</strong>
-            ${data.message}</p>
-          `,
+        replyTo: {
+          email:
+            data.email,
         },
-        {
-          headers: {
-            "api-key":
-              process.env
-                .BREVO_API_KEY,
 
-            accept:
-              "application/json",
+        subject:
+          "🚀 New Contact Form Submission",
 
-            "content-type":
-              "application/json",
-          },
-        }
-      );
+        htmlContent: `
+          <h2>
+            New Customer Inquiry
+          </h2>
 
-    console.log(
-      "Email Sent ✅"
+          <p>
+            <strong>Name:</strong>
+            ${data.name}
+          </p>
+
+          <p>
+            <strong>Email:</strong>
+            ${data.email}
+          </p>
+
+          <p>
+            <strong>Phone:</strong>
+            ${data.phone}
+          </p>
+
+          <p>
+            <strong>Service:</strong>
+            ${data.service}
+          </p>
+
+          <p>
+            <strong>Message:</strong>
+            ${data.message}
+          </p>
+        `,
+      },
+      {
+        headers: {
+          "api-key":
+            process.env
+              .BREVO_API_KEY,
+
+          accept:
+            "application/json",
+
+          "content-type":
+            "application/json",
+        },
+      }
     );
 
     console.log(
-      response.data
+      "Email Sent ✅"
     );
 
   } catch (error) {
